6659395f7ab51a966c1a3c45aa86e984991f829c
IndraStwann <sgngsgrt@gmail.com>
2022-09-15 02:55:45 +0700

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// const tombol1 = document.querySelector(".tombol1");
-// const Pesan2 = document.querySelector(".pesan_2");
-
-// tombol1.addEventListener("click", function () {
-//   console.log("ts berasil");
-//   Pesan2.style.display = "inline";
-// });
 const tombol = document.querySelectorAll(".tombol");
 const grub = document.querySelectorAll(".grub");
 
